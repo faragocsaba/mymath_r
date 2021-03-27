841e25742d428126139d6d6491388fd35a645be8
Csaba Farago <farago.csaba.phd@gmail.com>
2021-03-27 22:25:37 +0100

    Initial commit.

---
+++ R/myadd.R
@@ -0,0 +1,16 @@
+#' Adding two numbers
+#'
+#' This function just adds two numbers. It returns the result.
+#' Next line illustrating that this can be multiline.
+#'
+#' @param a the first number to be added
+#' @param b the second number to be added
+#' @return the sum of the two input values
+#' @author Csaba Farago
+#' @details
+#' This function adds two numbers.
+#' @export
+myadd <- function(a, b) {
+  result = a + b
+  return(result)
+}
---
+++ R/mymultiply.R
@@ -0,0 +1,15 @@
+#' Multiplying two numbers
+#'
+#' This function just multiplies two numbers. It returns the result.
+#'
+#' @param a the first number to be multiplied
+#' @param b the second number to be multiplied
+#' @return the prod of the two input values
+#' @author Csaba Farago
+#' @details
+#' This function multiplies two numbers.
+#' @export
+mymultiply <- function(a, b) {
+  result = a * b
+  return(result)
+}
